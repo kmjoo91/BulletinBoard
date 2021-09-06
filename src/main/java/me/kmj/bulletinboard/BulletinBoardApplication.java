425e0b77327ddc2b67e7bbcd2351af4b691ad6a5
kmjoo <kmjoo91@naver.com>
2021-09-07 04:43:08 +0900

    Gradle 프로젝트 생성

---
+++ src/main/java/me/kmj/bulletinboard/BulletinBoardApplication.java
@@ -0,0 +1,11 @@
+package me.kmj.bulletinboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinBoardApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BulletinBoardApplication.class, args);
+	}
+}
